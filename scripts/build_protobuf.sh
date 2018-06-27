67cdfe1bfec6ce4eaf11dc61a446d370b411ff1e
Roger Hsieh <chenyinh@uci.edu>
2018-06-27 14:17:44 -0700

    remove protobuf

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -21,6 +21,18 @@ rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 
+
+echo "cmake -DCMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
+      -DANDROID_NDK=${NDK_ROOT} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DANDROID_ABI="${ANDROID_ABI}" \
+      -DANDROID_NATIVE_API_LEVEL=21 \
+      -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 \
+      -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/protobuf \
+      -DBUILD_TESTING=OFF \
+      -Dprotobuf_BUILD_TESTS=OFF \
+      ../cmake"
+
 cmake -DCMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
       -DANDROID_NDK=${NDK_ROOT} \
       -DCMAKE_BUILD_TYPE=Release \
